6c87ca065dcf6e54339421b0349f424d1aa352a2
Virender.Khatri <virender.khatri@zuora.com>
2016-09-22 14:28:17 +0530

    #14, added https scheme support

--- main.go
+++ main.go
@@ -30,10 +30,11 @@ func (a ByCreateIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByCreateIndex) Less(i, j int) bool { return a[i].CreateIndex < a[j].CreateIndex }
 
 
-func backup(ipaddress string, token string, outfile string) {
+func backup(ipaddress string, scheme string, token string, outfile string) {
 
     config := api.DefaultConfig()
     config.Address = ipaddress
+    config.Scheme = scheme
     config.Token = token
 
 	client, _ := api.NewClient(config)
@@ -62,10 +63,11 @@ func backup(ipaddress string, token string, outfile string) {
     }
 }
 
-func backupAcls(ipaddress string, token string, outfile string) {
+func backupAcls(ipaddress string, scheme string, token string, outfile string) {
 
     config := api.DefaultConfig()
     config.Address = ipaddress
+    config.Scheme = scheme
     config.Token = token
 
 	client, _ := api.NewClient(config)
@@ -97,10 +99,11 @@ func backupAcls(ipaddress string, token string, outfile string) {
     KEY1:VALUE1
     KEY2:VALUE2
 */
-func restore(ipaddress string, token string, infile string) {
+func restore(ipaddress string, scheme string, token string, infile string) {
 
     config := api.DefaultConfig()
     config.Address = ipaddress
+    config.Scheme = scheme
     config.Token = token
 
     data, err := ioutil.ReadFile(infile)
@@ -134,20 +137,21 @@ func main() {
     usage := `Consul KV and ACL Backup with KV Restore tool.
 
 Usage:
-  consul-backup [-i IP:PORT] [-t TOKEN] [--aclbackup] [--aclbackupfile ACLBACKUPFILE] [--restore] <filename>
+  consul-backup [-i IP:PORT] [-s URI_SCHEME] [-t TOKEN] [--aclbackup] [--aclbackupfile ACLBACKUPFILE] [--restore] <filename>
   consul-backup -h | --help
   consul-backup --version
 
 Options:
   -h --help                          Show this screen.
   --version                          Show version.
-  -i, --address=IP:PORT              The HTTP endpoint of Consul [default: 127.0.0.1:8500].
+  -i, --address=IP:PORT              Consul endpoint address [default: 127.0.0.1:8500].
+  -s, --scheme=http|https            Consul endpoint URI Scheme [default: http].
   -t, --token=TOKEN                  An ACL Token with proper permissions in Consul [default: ].
   -a, --aclbackup                    Backup ACLs, does nothing in restore mode. ACL restore not available at this time.
   -b, --aclbackupfile=ACLBACKUPFILE  ACL Backup Filename [default: acl.bkp].
   -r, --restore                      Activate restore mode`
 
-  arguments, _ := docopt.Parse(usage, nil, true, "consul-backup 1.0", false)
+  arguments, _ := docopt.Parse(usage, nil, true, "consul-backup 1.1", false)
   fmt.Println(arguments)
 
   if arguments["--restore"] == true {
@@ -155,14 +159,14 @@ Options:
     fmt.Printf("Warning! This will overwrite existing kv. Press [enter] to continue; CTL-C to exit")
     fmt.Scanln()
     fmt.Println("Restoring KV from file: ", arguments["<filename>"].(string))
-    restore(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
+    restore(arguments["--address"].(string), arguments["--scheme"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
   } else {
     fmt.Println("Backup mode:")
     fmt.Println("KV store will be backed up to file: ", arguments["<filename>"].(string))
-    backup(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
+    backup(arguments["--address"].(string), arguments["--scheme"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
     if arguments["--aclbackup"] == true {
       fmt.Println("ACL Tokens will be backed up to file: ", arguments["--aclbackupfile"].(string))
-      backupAcls(arguments["--address"].(string), arguments["--token"].(string), arguments["--aclbackupfile"].(string))
+      backupAcls(arguments["--address"].(string), arguments["--scheme"].(string), arguments["--token"].(string), arguments["--aclbackupfile"].(string))
     }
   }
 }
